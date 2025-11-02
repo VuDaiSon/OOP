4605b9f1fb3bb5c4353abb1138874f2ad84a632e
VuDaiSon <cocokoco108@gmail.com>
2025-11-02 22:26:58 +0700

    some basic exercises

---
+++ src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/ReverseString.java
@@ -0,0 +1,31 @@
+package lab2.java.basic.exercises.on.string.andchar.operations;
+
+import java.util.Scanner;
+
+// Write a method called reverseString(String inStr) (with prototype String reverseString(String inStr))
+// which reverses the string inStr by extracting and processing each character.
+// Write a method called testReverseString() which prompts user for a string,
+// and prints the reverse of the string using the method reverseString().
+public class ReverseString {
+
+    public static String reverseString(String inStr) {
+        int inStrLen = inStr.length();
+        StringBuilder reversedStr = new StringBuilder();
+        for (int charIdx = inStrLen - 1; charIdx >= 0; --charIdx) {
+            reversedStr.append(inStr.charAt(charIdx));
+        }
+        return reversedStr.toString();
+    }
+
+    public static void testReverseString() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String inStr = in.next();
+        String reversed = reverseString(inStr);
+        System.out.println("The reverse of the String \"" + inStr + "\" is \"" + reversed + "\".");
+    }
+
+    public static void main(String[] args) {
+        testReverseString();
+    }
+}
