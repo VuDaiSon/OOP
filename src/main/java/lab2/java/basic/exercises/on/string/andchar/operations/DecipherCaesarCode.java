540a608648fa8c7e7a9655ade7693a47e67880b6
VuDaiSon <cocokoco108@gmail.com>
2025-11-07 15:04:56 +0700

    some basic exercises

--- src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/DecipherCaesarCode.java
+++ src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/DecipherCaesarCode.java
@@ -1,5 +1,11 @@
 package lab2.java.basic.exercises.on.string.andchar.operations;
 
+/**
+ * Write a method called decipherCaesarCode() (with prototype String decipherCaesarCode(String
+ * inStr)) to decipher the Caesar’s code described in the previous exercise. Write a method
+ * called testDecipherCaesarCode(), which prompts user for a ciphertext string consisting of
+ * mix-case letters only; compute the plaintext; and print the plaintext in uppercase.
+ */
 public class DecipherCaesarCode {
     public static void main(String[] args) {
 
