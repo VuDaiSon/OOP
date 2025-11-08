649d951af78c3903185ed16f1d64b91b13464226
VuDaiSon <cocokoco108@gmail.com>
2025-11-08 13:47:36 +0700

    some basic exercises

--- src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/CaesarCode.java
+++ src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/CaesarCode.java
@@ -1,8 +1,37 @@
 package lab2.java.basic.exercises.on.string.andchar.operations;
 
 
+import java.util.Scanner;
+
 public class CaesarCode {
     public static void main(String[] args) {
+        testCipherCaesarCode();
+    }
+
+    public static String cipherCaesarCode(String inStr) {
+        inStr = inStr.toUpperCase();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < inStr.length(); i++) {
+            char inChar = inStr.charAt(i);
+            if (inChar >= 'A' && inChar <= 'W') {
+                result.append((char) (inChar + 3));
+            } else if (inChar == 'X') {
+                result.append('A');
+            } else if (inChar == 'Y') {
+                result.append('B');
+            } else if (inChar == 'Z') {
+                result.append('C');
+            } else {
+                result.append(inChar);
+            }
+        }
+        return result.toString();
+    }
+    public static void testCipherCaesarCode(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a plaintext string: ");
+        String inStr = in.nextLine();
+        System.out.print("The ciphertext string is: " + cipherCaesarCode(inStr));
 
     }
 }
