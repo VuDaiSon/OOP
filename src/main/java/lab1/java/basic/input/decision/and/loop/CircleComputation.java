b0d9262a00f98cc24ef451f20b3dbb83c7e83997
VuDaiSon <cocokoco108@gmail.com>
2025-11-11 15:47:54 +0700

    some basic exercises

--- src/main/java/lab1/java/basic/input/decision/and/loop/CircleComputation.java
+++ src/main/java/lab1/java/basic/input/decision/and/loop/CircleComputation.java
@@ -2,10 +2,12 @@ package lab1.java.basic.input.decision.and.loop;
 
 import java.util.Scanner;
 
-// Write a program called CircleComputation that prompts user for the radius of a circle in
-// floating point number. The program shall read the input as double compute the diameter,
-// circumference, and area of the circle in double and print the values rounded to 2 decimal places.
-// Use System-provided constant Math.PI for pi.
+/**
+ Write a program called CircleComputation that prompts user for the radius of a circle in
+ floating point number. The program shall read the input as double compute the diameter,
+ circumference, and area of the circle in double and print the values rounded to 2 decimal places.
+ Use System-provided constant Math.PI for pi.
+ */
 public class CircleComputation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
