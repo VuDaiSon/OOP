30e2865ce7b8dc4faf8ac7d197ce3bb3004619de
VuDaiSon <cocokoco108@gmail.com>
2025-11-15 02:11:30 +0700

    some basic exercises

--- src/main/java/lab1/java/basic/input/decision/and/loop/ReverseInt.java
+++ src/main/java/lab1/java/basic/input/decision/and/loop/ReverseInt.java
@@ -3,8 +3,11 @@ package lab1.java.basic.input.decision.and.loop;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-//Write a program that prompts user for a positive integer.
-// The program shall read the input as int; and print the ”reverse” of the input integer.
+/**
+Write a program that prompts user for a positive integer.
+ The program shall read the input as int; and print the ”reverse” of the input integer.
+ */
+
 public class ReverseInt {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
