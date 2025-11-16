d160855db9387aecf136a069a85b03973be031a3
VuDaiSon <cocokoco108@gmail.com>
2025-11-16 22:22:45 +0700

    some basic exercises

--- src/main/java/lab1/java/basic/input/decision/and/loop/SumProductMinMax3.java
+++ src/main/java/lab1/java/basic/input/decision/and/loop/SumProductMinMax3.java
@@ -1,10 +1,11 @@
 package lab1.java.basic.input.decision.and.loop;
 
 import java.util.Scanner;
-
-// Write a program called SumProductMinMax3 that prompts user for three integers.
-// The program shall read the inputs as int;
-// compute the sum, product, minimum and maximum of the three integers; and print the results.
+/**
+ Write a program called SumProductMinMax3 that prompts user for three integers.
+ The program shall read the inputs as int;
+ compute the sum, product, minimum and maximum of the three integers; and print the results.
+ */
 public class SumProductMinMax3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
