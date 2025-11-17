0431502f5760356dbd8d262a4c8757c42ab644b5
VuDaiSon <cocokoco108@gmail.com>
2025-11-18 00:25:08 +0700

    some basic exercises

--- src/main/java/lab1/java/basic/exercises/on/decision/and/loop/ExtractDigits.java
+++ src/main/java/lab1/java/basic/exercises/on/decision/and/loop/ExtractDigits.java
@@ -3,9 +3,11 @@ package lab1.java.basic.exercises.on.decision.and.loop;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-//Write a program called ExtractDigits to extract each digit from an int, in the reverse order.
-//For example, if the int is 15423, the output shall be ”3 2 4 5 1”, with a space separating the
-//digits.
+/**
+Write a program called ExtractDigits to extract each digit from an int, in the reverse order.
+For example, if the int is 15423, the output shall be ”3 2 4 5 1”, with a space separating the
+digits.
+ */
 public class ExtractDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
