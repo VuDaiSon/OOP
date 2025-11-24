9168992d7affd8ad2ca36ecc1f1aaf7172509e9c
VuDaiSon <cocokoco108@gmail.com>
2025-11-24 23:52:45 +0700

    some basic exercises

--- src/main/java/lab1/java/basic/exercises/on/decision/and/loop/SumAverageRunningInt.java
+++ src/main/java/lab1/java/basic/exercises/on/decision/and/loop/SumAverageRunningInt.java
@@ -1,7 +1,9 @@
 package lab1.java.basic.exercises.on.decision.and.loop;
-// Write a program called SumAverageRunningInt to produce the sum of 1, 2, 3, . . . , to 100.
-// Store 1 and 100 in variables lowerbound and upperbound, so that we can change their values easily
-// Also compute and display the average.
+/**
+ Write a program called SumAverageRunningInt to produce the sum of 1, 2, 3, . . . , to 100.
+ Store 1 and 100 in variables lowerbound and upperbound, so that we can change their values easily
+ Also compute and display the average.
+ */
 public class SumAverageRunningInt {
     public static void main(String[] args) {
         int sum = 0;
