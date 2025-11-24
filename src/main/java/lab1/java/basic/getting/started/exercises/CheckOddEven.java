49583f325441366821115e1e42c1de644c9efa13
VuDaiSon <cocokoco108@gmail.com>
2025-11-25 00:18:18 +0700

    some basic exercises

--- src/main/java/lab1/java/basic/getting/started/exercises/CheckOddEven.java
+++ src/main/java/lab1/java/basic/getting/started/exercises/CheckOddEven.java
@@ -2,9 +2,10 @@ package lab1.java.basic.getting.started.exercises;
 
 import java.math.BigInteger;
 import java.util.Scanner;
-
-//Write a program called CheckOddEven which prints ”Odd Number” if the int variable “number” is odd
-//or “Even Number” otherwise. The program shall always print “Bye!” before exiting.
+/**
+Write a program called CheckOddEven which prints ”Odd Number” if the int variable “number” is odd
+or “Even Number” otherwise. The program shall always print “Bye!” before exiting.
+ */
 public class CheckOddEven {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
