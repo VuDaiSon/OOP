bc7dd1dd5f6915132cf9403546637ab853727d9a
VuDaiSon <cocokoco108@gmail.com>
2025-12-12 23:48:41 +0700

    some basic exercises

--- src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/RadixNToDecimal.java
+++ src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/RadixNToDecimal.java
@@ -2,6 +2,9 @@ package lab2.java.basic.exercises.on.string.andchar.operations;
 
 public class RadixNToDecimal {
     public static void main(String[] args) {
+test();
+    }
+    public static void test(){
 
     }
 }
