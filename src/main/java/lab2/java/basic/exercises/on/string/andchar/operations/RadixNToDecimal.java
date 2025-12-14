a0718beeaff3746f187292076d4aa14c27ca6b78
VuDaiSon <cocokoco108@gmail.com>
2025-12-14 15:47:40 +0700

    some basic exercises

--- src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/RadixNToDecimal.java
+++ src/main/java/lab2/java/basic/exercises/on/string/andchar/operations/RadixNToDecimal.java
@@ -1,5 +1,11 @@
 package lab2.java.basic.exercises.on.string.andchar.operations;
 
+/**
+ * Write a method called radixNToDecimal() (with prototype int radixNToDecimal(StringradixNStr))
+ * to convert an input string of any radix (≤ 16) into its equivalent decimal number.
+ * Write a method called testRadixNToDecimal(), which prompts user for a radix n string,
+ * and convert the input radix n string into its equivalent decimal number.
+ */
 public class RadixNToDecimal {
     public static void main(String[] args) {
         test();
