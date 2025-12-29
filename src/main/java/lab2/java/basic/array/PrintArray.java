768b7072f7d33085582d054b4d86e6ab97055e51
VuDaiSon <cocokoco108@gmail.com>
2025-12-29 23:18:51 +0700

    some basic exercises

--- src/main/java/lab2/java/basic/array/PrintArray.java
+++ src/main/java/lab2/java/basic/array/PrintArray.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class PrintArray {
     public static void main(String[] args) {
-
         test();
     }
     public int[] createArray(Scanner in){
-        System.out.println("Enter the number o f i tem s :");
+        System.out.println("Enter the number of item s :");
     }
     public static void test(){
 
