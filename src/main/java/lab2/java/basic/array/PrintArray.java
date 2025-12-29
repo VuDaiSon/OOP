52da1f22d9085bbe2c7a190d5eedde7ab9993a4f
VuDaiSon <cocokoco108@gmail.com>
2025-12-29 23:18:28 +0700

    some basic exercises

--- src/main/java/lab2/java/basic/array/PrintArray.java
+++ src/main/java/lab2/java/basic/array/PrintArray.java
@@ -1,8 +1,14 @@
 package lab2.java.basic.array;
 
+import java.util.Scanner;
+
 public class PrintArray {
     public static void main(String[] args) {
+
         test();
+    }
+    public int[] createArray(Scanner in){
+        System.out.println("Enter the number o f i tem s :");
     }
     public static void test(){
 
