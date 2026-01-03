add359e87a33692bd206216ef08cc52192b2fa60
VuDaiSon <cocokoco108@gmail.com>
2026-01-04 02:25:16 +0700

    some basic exercises

---
+++ src/main/java/lab2/java/basic/method/MagicSum.java
@@ -0,0 +1,11 @@
+package lab2.java.basic.method;
+
+/**
+ * Write a boolean method called hasEight(), which takes an int as input and returns true if the
+ * number contains the digit 8 (e.g., 18, 168, 1288). The signature of the method is as follows:
+ * public static boolean hasEight (int number);
+ * Write a method called int testMagicSum(Scanner in) which prompts user for integers (or −1 to end),
+ * and produce the sum of numbers containing the digit 8. Your program should use the above methods.
+ */
+public class MagicSum {
+}
--- src/main/java/lab2/java/basic/method/MethodExercise.java
+++ src/main/java/lab2/java/basic/method/MethodExercise.java
@@ -11,10 +11,23 @@ import java.util.Scanner;
  */
 public class MethodExercise {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        testExponent(in);
+        in.close();
     }
     public static void testExponent(Scanner in){
-        int exp;
-        int base;
-        
+        System.out.print("Enter the base: ");
+        int base = in.nextInt();
+        System.out.print("Enter the exponent: ");
+        int exp = in.nextInt();
+        System.out.println(base + " raises to the power of " + exp + "is: " + exponent(base, exp));
+    }
+    public static int exponent(int base, int exp){
+        int product = 1;
+        for(int i = 0; i<exp; i++){
+            product *=base;
+        }
+        return product;
+
     }
 }
