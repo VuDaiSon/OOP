2c840d3f24f73cec8129065132ee70e072f202f1
VuDaiSon <cocokoco108@gmail.com>
2026-01-03 23:39:25 +0700

    some basic exercises

--- src/main/java/lab2/java/basic/array/DecimalToHexadecimal.java
+++ src/main/java/lab2/java/basic/array/DecimalToHexadecimal.java
@@ -1,5 +1,7 @@
 package lab2.java.basic.array;
 
+import java.util.Scanner;
+
 /**
  * Write a method called decimalToHexadecimal() (with prototype String decimalToHexadeci-
  * mal(int positiveInteger)) to convert a positive decimal number into its equivalent hexadecimal
@@ -7,4 +9,33 @@ package lab2.java.basic.array;
  * decimal number, read as int, and print its equivalent hexadecimal string.
  */
 public class DecimalToHexadecimal {
+    public static void main(String[] args) {
+        testDecimalToHexadecimal();
+    }
+    public static String decimalToHexadecimal(int positiveInteger){
+        if(positiveInteger == 0){
+            return "0";
+        }
+        StringBuilder hex = new StringBuilder();
+        while(positiveInteger >0){
+            char hexChar;
+            int remainder = positiveInteger % 16;
+            if(remainder < 10){
+                hexChar = (char) ('0' + remainder);
+            }
+            else{
+                hexChar = (char) ('A' + remainder - 10);
+            }
+            hex.insert(0, hexChar);
+            positiveInteger /=16;
+        }
+        return hex.toString();
+    }
+    public static void testDecimalToHexadecimal(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        int num = in.nextInt();
+        String hex = decimalToHexadecimal(num);
+        System.out.println("The equivalent hexadecimal number is " + hex);
+    }
 }
---
+++ src/main/java/lab2/java/basic/method/MethodExercise.java
@@ -0,0 +1,20 @@
+package lab2.java.basic.method;
+
+import java.util.Scanner;
+
+/**
+ * Write a method called exponent(int base, int exp) that returns an int value of base raises to
+ * the power of exp. The signature of the method is:
+ * public static int exponent (int base, int exp);
+ * Assume that exp is a non-negative integer and base is an integer. Do not use any Math library functions.
+ * Also write the testExponent() method that prompts user for the base and exp; and prints the result.
+ */
+public class MethodExercise {
+    public static void main(String[] args) {
+    }
+    public static void testExponent(Scanner in){
+        int exp;
+        int base;
+        
+    }
+}
