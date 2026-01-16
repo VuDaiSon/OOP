025544bd9e5e5f87b7bb0bfaeeee088b5d6fcd76
VuDaiSon <cocokoco108@gmail.com>
2026-01-16 23:46:11 +0700

    some basic exercises

--- src/main/java/homework2/nested_loops/CheckerPattern.java
+++ src/main/java/homework2/nested_loops/CheckerPattern.java
@@ -1,4 +1,12 @@
 package homework2.nested_loops;
 
+/**
+ * Write a method called checkerPattern() (with prototype void checkerPattern(int n)) that
+ * prints the following checkerboard pattern. Write a method called testCheckerPattern() that
+ * prompts user for the size (a non-negative integer in int); and prints the checkerboard pattern.
+ */
 public class CheckerPattern {
+    public static void main(String[] args) {
+
+    }
 }
